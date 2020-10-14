ade230b42e9619a11e31e6252a9aca8b2bc1f054
carolina <carolinacapetilloc@gmail.com>
2020-10-14 13:54:55 -0700

    adds order button, not yet styled

--- src/components/Reserve/reserve.jsx
+++ src/components/Reserve/reserve.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './Reserve.css';
 import plain from '../Images/plain.jpg';
-import plain2 from '../Images/plain2.jpg';
 import poppyseed from '../Images/poppyseed.jpg';
-import poppyseed2 from '../Images/poppyseed2.jpg';
 import sesame from '../Images/sesame.jpg';
-import sesame2 from '../Images/sesame2.jpg';
 import zaatar from '../Images/zaatar.jpg';
-import zaatar2 from '../Images/zaatar2.jpg'
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Button} from 'react-bootstrap';
 
 const Reserve = (props) => {
     return (
@@ -19,7 +15,6 @@ const Reserve = (props) => {
                 </div>
             </Row>
             <Row>
-               
                     <Col sm={12} md={6} className="d-flex align-items-center justify-content-center pt-3">
                         <div className="plain">
                             <img src={plain} alt="plain challah" id="plain"/> 
@@ -30,7 +25,6 @@ const Reserve = (props) => {
                             <img src={poppyseed} alt="poppyseed challah" id="poppyseed"/>
                         </div> 
                     </Col>
-             
             </Row>
             <Row>
                 <Col sm={12} md={6} className="d-flex align-items-center justify-content-center py-3">
@@ -42,6 +36,15 @@ const Reserve = (props) => {
                     <div className="zaatar">
                         <img src={zaatar} alt="za'atar challah" id="zaatar"/>   
                     </div>
+                </Col>
+            </Row>
+            <Row> 
+                <Col className="d-flex align-items-center justify-content-center p-5">
+                    <a href="https://gmail.us2.list-manage.com/subscribe?u=6a90ee103a39a8ea6e4b2d543&id=6cdfa69eb3" target="blank">
+                        <Button className="collapsed justify-self-center" type="button">
+                            order
+                        </Button>
+                    </a>
                 </Col>
             </Row>
         </div>
