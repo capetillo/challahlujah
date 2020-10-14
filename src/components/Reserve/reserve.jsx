09071cf53263f4e9a29fc6b1f79031434504de5c
carolina <carolinacapetilloc@gmail.com>
2020-10-13 19:06:59 -0700

    plain challah works on hover

--- src/components/Reserve/reserve.jsx
+++ src/components/Reserve/reserve.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Reserve.css';
 import plain from '../Images/plain.jpg';
+import plain2 from '../Images/plain2.jpg';
 import poppyseed from '../Images/poppyseed.jpg';
+import poppyseed2 from '../Images/poppyseed2.jpg';
 import sesame from '../Images/sesame.jpg';
-import zaatar from '../Images/zaatar.jpg'
-import {Row, Col, Image} from 'react-bootstrap';
+import sesame2 from '../Images/sesame2.jpg';
+import zaatar from '../Images/zaatar.jpg';
+import zaatar2 from '../Images/zaatar2.jpg'
+import {Row, Col} from 'react-bootstrap';
 
 const Reserve = (props) => {
     return (
@@ -16,18 +20,26 @@ const Reserve = (props) => {
             </Row>
             <Row>
                 <Col sm={12} md={6} className="d-flex align-items-center justify-content-center pt-3">
-                    <Image src={plain} alt="plain challah" id="plain"/>    
+                    <div className="plain">
+                    <img src={plain} alt="plain challah" id="plain"/> 
+                    </div>
+                    <div className="plain2">
+                    <img src={plain2} alt="plain challah" id="plain2"/> 
+                    </div>  
                 </Col>
                 <Col sm={12} md={6} className="d-flex align-items-center justify-content-center pt-3">
-                    <Image src={poppyseed} alt="poppyseed challah" id="poppyseed"/>
+                    <img src={poppyseed} alt="poppyseed challah" id="poppyseed"/>
+                    <img src={poppyseed2} alt="poppyseed challah" id="poppyseed2"/>
                 </Col>
             </Row>
             <Row>
                 <Col sm={12} md={6} className="d-flex align-items-center justify-content-center py-3">
-                    <Image src={sesame} alt="sesame challah" id="sesame"/>
+                    <img src={sesame} alt="sesame challah" id="sesame"/>
+                    <img src={sesame2} alt="sesame challah" id="sesame2"/>
                 </Col>
                 <Col sm={12} md={6} className="d-flex align-items-center justify-content-center py-3">
-                    <Image src={zaatar} alt="za'atar challah" id="zaatar"/>    
+                    <img src={zaatar} alt="za'atar challah" id="zaatar"/>  
+                    <img src={zaatar2} alt="za'atar challah" id="zaatar2"/>  
                 </Col>
             </Row>
         </div>
