d918ee3051d86d46351129fd262ef5888c98e911
carolina <carolinacapetilloc@gmail.com>
2020-10-14 14:50:44 -0700

    fixes nav bar issue where it was covering header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Header.css';
+import {Row, Col} from 'react-bootstrap';
 
 const Header = (props) => {
     return (
         <div className="header">
-            <p className="challah-by-ari">challah by ari</p>
+            <Row className="mt-5">
+                <Col className="mt-5">
+                    <p className="challah-by-ari">challah by ari</p>
+                </Col>
+            </Row>
         </div>
     )
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -32,7 +32,7 @@ class Navigation extends Component {
         render () {
             return (
         <div className="links" id="navbar">
-            <Nav className="navbar navbar-expand-md navbar-dark shadow">
+            <Nav className="navbar navbar-expand-md navbar-dark shadow p-0">
             <div className="menu-links">
             <Button id ="boton" className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span id="line1" className="line"></span> 
