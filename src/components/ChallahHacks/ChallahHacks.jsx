74936b09e236d5c48f9fbc1370567877a751ffd3
carolina <carolinacapetilloc@gmail.com>
2020-11-04 16:49:01 -0800

    font issues againgit add .git add .

--- src/components/ChallahHacks/ChallahHacks.jsx
+++ src/components/ChallahHacks/ChallahHacks.jsx
@@ -18,25 +18,25 @@ const ChallahHacks = (props) => {
                     <Col md={1}></Col>
                     <Col md={5}>
                     <Col xs={12} className="px-5">
-                            <p className="subheader">almost like you made it at home</p>
+                            <h3 className="subheader">almost like you made it at home</h3>
                         </Col>
                         <Col xs={12} className="px-5 pb-md-5">
                             <p className="hack-context">to freshen up the loaf before eating, wrap it in foil and place it in a hot oven (200-300 degrees) for about 15 minutes or until warmed through.</p>
                         </Col>
                         <Col xs={12} className="px-5">
-                            <p className="subheader">the freezer is your friend</p>
+                            <h3 className="subheader">the freezer is your friend</h3>
                         </Col>
                         <Col xs={12} className="px-5 pb-md-5">
                             <p className="hack-context">challah freezes really well! you can place an entire loaf in the freezer or slice it before so you can toast off individual pieces whenever you have a craving. always put it in sealed plastic before freezing. to defrost the entire loaf I like to wrap mine in aluminum foil and place in a low heat oven until warm all the way through.</p>
                         </Col>
                         <Col xs={12} className="px-5">
-                            <p className="subheader">best french toast of your life</p>
+                            <h3 className="subheader">best french toast of your life</h3>
                         </Col>
                         <Col xs={12} className="px-5 pb-md-5">
                             <p className="hack-context">did not come up with this one but do think it's the greatest thing ever. challah works especially well for french toast when it's not at its most fresh.</p>
                         </Col>
                         <Col xs={12} className="px-5">
-                            <p className="subheader">challah in your salad: the crouton disguise</p>
+                            <h3 className="subheader">challah in your salad: the crouton disguise</h3>
                         </Col>
                         <Col xs={12} className="px-5 pb-md-5">
                             <p className="hack-context">leftover challah chopped into small cubes and covered in olive oil, salt, and pepper will be the greatest addition to your next salad. bake around 350 until toasted and crunchy. you're welcome.</p>
